7550034cb2f7c4d1b0a7ff8e9ba10cf2ed729d5d
kether667 <lkarolewski@gmail.com>
2015-07-21 14:25:21 -0700

    added ui-router

--- src/config.js
+++ src/config.js
@@ -16,9 +16,13 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.4.3",
+    "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
     "babel": "npm:babel-core@5.7.4",
     "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.18",
+    "github:angular-ui/ui-router@0.2.15": {
+      "angular": "github:angular/bower-angular@1.4.3"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,13 +1,30 @@
 import angular from 'angular';
+import 'angular-ui-router';
+
 import RootCtrl from 'js/app/controllers/rootCtrl';
 import OtherCtrl from 'js/app/controllers/otherCtrl';
 
 export default function bootstrap() {
     angular.element(document).ready(function () {
 
-        angular.module('app', [])
-            .controller('rootCtrl', RootCtrl)
-            .controller('otherCtrl', OtherCtrl);
+        angular.module('app', ['ui.router'])
+            .config(function ($stateProvider, $urlRouterProvider) {
+                $stateProvider
+                    .state('state1', {
+                        url: '/state1',
+                        template: '<div>list <button ng-click="ctrl.hello()">hello</button><button ui-sref="state2">go to state 2</button></div>',
+                        controller: OtherCtrl,
+                        controllerAs: 'ctrl'
+                    })
+                    .state('state2', {
+                        url: '/state2',
+                        template: '<div>list <button ng-click="ctrl.hello()">hello</button><button ui-sref="state1">go to state 1</button></div>',
+                        controller: RootCtrl,
+                        controllerAs: 'ctrl'
+                    });
+
+                $urlRouterProvider.otherwise('/state1');
+            });
 
         angular.bootstrap(document, ['app']);
     });
