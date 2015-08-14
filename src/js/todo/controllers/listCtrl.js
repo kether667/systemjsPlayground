55fb6421a95a2cd5d8f766aec2600f8a48da3896
kether667 <lkarolewski@gmail.com>
2015-08-13 17:21:00 -0700

    facebook interview question

--- src/js/todo/controllers/listCtrl.js
+++ src/js/todo/controllers/listCtrl.js
@@ -22,4 +22,45 @@ export default class ListCtrl {
     openDatePicker () {
         this.opened = true;
     }
+
+    solve(list) {
+        var arr = [3, 3, 4, 5, 5];
+
+        this.items.push(this.findUnique(arr));
+    }
+
+    findUnique(arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        var middle = (arr.length - 1) / 2;
+
+        var leftPart = arr.slice(0, middle - 1);
+        var rightPart = arr.slice(middle + 1, arr.length);
+
+        if (arr[middle] == arr[middle - 1]) {
+            if (leftPart.length % 2 === 0) {
+                return this.findUnique(rightPart);
+            }
+            if (leftPart.length % 2 === 1) {
+                return this.findUnique(leftPart);
+            }
+
+        } else if (arr[middle] == arr[middle + 1]) {
+
+            var leftPart = arr.slice(0, middle - 1);
+            var rightPart = arr.slice(middle + 2, arr.length - 1);
+
+            if (leftPart.length % 2 === 0) {
+                return this.findUnique(rightPart);
+
+                if (leftPart.length % 2 === 1) {
+                    return this.findUnique(leftPart);
+                }
+            } else {
+                return arr[middle];
+            }
+        }
+    }
 }
