627b1ad1c4819aabf9c280b30638be94dce3154e
kether667 <lkarolewski@gmail.com>
2015-10-12 14:32:06 -0700

    updated to lastes version of libs to check if it still works ;)

--- src/config.js
+++ src/config.js
@@ -1,44 +1,60 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime"
     ]
   },
-  "paths": {
+  paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
-    "angular": "github:angular/bower-angular@1.4.3",
-    "angular-bootstrap": "github:angular-ui/bootstrap-bower@0.13.1",
-    "angular-mocks": "github:angular/bower-angular-mocks@1.4.3",
+  map: {
+    "angular": "github:angular/bower-angular@1.4.7",
+    "angular-bootstrap": "github:angular-ui/bootstrap-bower@0.13.4",
+    "angular-mocks": "github:angular/bower-angular-mocks@1.4.7",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
-    "babel": "npm:babel-core@5.7.4",
-    "babel-runtime": "npm:babel-runtime@5.7.0",
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@0.9.18",
     "github:angular-ui/ui-router@0.2.15": {
-      "angular": "github:angular/bower-angular@1.4.3"
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:angular/bower-angular-mocks@1.4.3": {
-      "angular": "github:angular/bower-angular@1.4.3"
+    "github:angular/bower-angular-mocks@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
     },
-    "npm:babel-runtime@5.7.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.25": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
-
